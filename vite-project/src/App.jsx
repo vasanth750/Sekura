506a297fa2ac00796f65c312c14c2ffec6b23ebd
Vasanthakannan R <24ugcos00014@drngpasc.ac.in>
2026-05-11 15:54:57 +0530

    branch creted

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Signup from './Pages/Signup.jsx';
-import Login from './Pages/Login.jsx'
+import Login from './Pages/Login.jsx';
+import footer from './components/footer/footer.jsx';
+
 
 function App() {
   return (
+    <>
     <div>
       <Routes>
 
@@ -13,7 +16,10 @@ function App() {
         <Route path="/Signup" element={<Signup />} />
 
       </Routes>
+      <footer />
     </div>
+    
+    </>
   );
 }
 
---
+++ vite-project/src/components/footer/footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer () {
+    return (
+        <>
+        <footer>
+            <p>
+                ©Sekura inc  All rights reserved
+            </p>
+
+            <p>
+                Privacy Policy  Terms and Conditions
+            </p>
+            
+            <p>
+                End to End Encrypted | V 0.01
+
+            </p>
+        </footer>
+        </>
+    );
+}
